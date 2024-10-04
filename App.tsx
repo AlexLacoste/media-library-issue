a01b11a77b91cb4d7e999b9c23c53efce0de4b44
Alexandre Lacoste <alexlacoste3@gmail.com>
2024-10-04 11:10:06 +0200

    Add video support: integrate MediaLibrary, display videos with FlatList, and include permissions management

--- App.tsx
+++ App.tsx
@@ -1,12 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import * as MediaLibrary from 'expo-media-library';
+import { useEffect, useState } from 'react';
+import { Video } from 'expo-av';
+import { Image } from 'expo-image';
 
 export default function App() {
+  const [videos, setVideos] = useState<(MediaLibrary.Asset & {localUri?: string})[]>([]);
+  const [activeVideo, setActiveVideo] = useState<(MediaLibrary.Asset & {localUri?: string})>();
+  const [permissionResponse, requestPermission] = MediaLibrary.usePermissions();
+
+  useEffect(() => {
+    const getVideos = async () => {
+      if (permissionResponse?.status !== 'granted') {
+        await requestPermission();
+      }
+      const videos = await MediaLibrary.getAssetsAsync({
+        mediaType: 'video',
+        first: 10,
+      });
+      const videosInfo = await Promise.all(videos.assets.map(async (video) => await MediaLibrary.getAssetInfoAsync(video.id)));
+      setVideos(videosInfo);
+    };
+    getVideos();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList horizontal data={videos} renderItem={({ item }) => 
+        <TouchableOpacity onPress={() => {setActiveVideo(item)}}>
+          <Image source={{ uri: item.uri }} style={{ width: 100, height: 100 }}  />
+        </TouchableOpacity>} />
+        <Text>{activeVideo?.localUri}</Text>
+        {activeVideo && <Video source={{ uri: activeVideo.localUri ?? '' }} style={{ width: 300, height: 300 }} shouldPlay />}
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
